d315ff044c3b6540d850cb55246b201d7d926d2d
Raja 👽👾🤖 <126279774+Rabenarivo@users.noreply.github.com>
2026-06-27 13:28:02 +0300

    login

--- clinique/accounts/views.py
+++ clinique/accounts/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def home_view(request):
+    if request.user.is_authenticated:
+        profile = getattr(request.user, 'profile', None)
+        return render(request, 'home.html', {
+            'user': request.user,
+            'profile': profile,
+        })
+    return redirect('login')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm(request)
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
